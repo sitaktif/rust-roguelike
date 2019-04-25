1f158c2f30a90d6e3c59d8a8395446cd2d6638cd
Romain Chossart <romainchossart@gmail.com>
2019-04-25 21:59:19 +0100

    Add walls and floor

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,59 @@ const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x, y, char, color,
+        }
+    }
+    /// Move object by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if map[(self.x + dx) as usize][(self.y + dy) as usize].traversable {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    traversable: bool,
+    transparent: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { traversable: true, transparent: true }
+    }
+
+    pub fn wall() -> Self {
+        Tile { traversable: false, transparent: false }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
 fn main() {
     println!("Hello, world!");
 
@@ -27,13 +80,11 @@ fn main() {
     let npc = Object::new(player.x - 1, player.y -3, '@', colors::YELLOW);
 
     let mut objects = [player, npc];
+    let map = make_map();
 
     while !root.window_closed() {
         con.set_default_foreground(colors::WHITE);
-        for obj in objects.iter() {
-            obj.draw(&mut con);
-        }
-        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+        render_all(&mut root, &mut con, &objects, &map);
 
         root.flush();
         for obj in objects.iter() {
@@ -42,7 +93,7 @@ fn main() {
 
         // Handle keys and exit if needed
         let player = &mut objects[0];
-        let exit = handle_keys(&mut root, player);
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break;
         }
@@ -50,31 +101,35 @@ fn main() {
 
 }
 
-struct Object {
-    x: i32,
-    y: i32,
-    char: char,
-    color: Color,
-}
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    // Draw all the objects in the list
+    for obj in objects {
+        obj.draw(con)
+    }
 
-impl Object {
-    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-        Object {
-            x, y, char, color,
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let opaque = !map[x as usize][y as usize].transparent;
+            if opaque {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
         }
     }
-    /// Move object by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
-    }
-    pub fn draw(&self, con: &mut Console) {
-        con.set_default_foreground(self.color);
-        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
-    }
-    pub fn clear(&self, con: &mut Console) {
-        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
-    }
+    // Overlay the console over the root
+    blit(con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
+
+fn make_map() -> Map {
+    // Fill map with traversable tiles
+    // vec![ITEM;NUM] is a macro to create a Vec of size NUM filled with ITEM (where ITEM is
+    // evaluated at each iteration).
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+    map
 }
 
 /// Handle a key press event
@@ -82,16 +137,16 @@ impl Object {
 /// # Return value
 ///
 /// A value of true means that the caller should exit.
-fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     // TODO
     let key = root.wait_for_keypress(true);
 
     match key {
         // Player movement
-        Key { code: Up, .. } | Key { printable: 'k', .. } => player.move_by(0, -1),
-        Key { code: Down, .. } | Key { printable: 'j', .. }  => player.move_by(0, 1),
-        Key { code: Left, .. } | Key { printable: 'h', .. }  => player.move_by(-1, 0),
-        Key { code: Right, .. } | Key { printable: 'l', .. }  => player.move_by(1, 0),
+        Key { code: Up, .. } | Key { printable: 'k', .. } => player.move_by(0, -1, map),
+        Key { code: Down, .. } | Key { printable: 'j', .. }  => player.move_by(0, 1, map),
+        Key { code: Left, .. } | Key { printable: 'h', .. }  => player.move_by(-1, 0, map),
+        Key { code: Right, .. } | Key { printable: 'l', .. }  => player.move_by(1, 0, map),
 
         // Alt-enter: toggle fullscreen
         Key { code: Enter, alt: true, .. } => {
