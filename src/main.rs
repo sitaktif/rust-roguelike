51d64a07e56722d2367dde90fbd9e5791b06adc4
Romain Chossart <romainchossart@gmail.com>
2019-04-28 14:01:47 +0100

    Add room generator

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate tcod;
 
 use std::cmp::*;
 
+use rand::Rng;
+
 use tcod::console::*;
 use tcod::colors::{self,Color};
 use tcod::input::Key;
@@ -9,10 +11,14 @@ use tcod::input::KeyCode::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
-const LIMIT_FPS: i32 = 20;
+const LIMIT_FPS: i32 = 40;
 
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
@@ -76,6 +82,14 @@ impl Rect {
     pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
         Rect { x1: x, y1: y, x2: x + w, y2: y + h }
     }
+    pub fn center(&self) -> (i32, i32) {
+        ((self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2)
+    }
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        let x_intersects: bool = self.x2 >= other.x1 && self.x1 <= other.x2;
+        let y_intersects: bool = self.y2 >= other.y1 && self.y1 <= other.y2;
+        x_intersects && y_intersects
+    }
 }
 
 fn main() {
@@ -92,11 +106,12 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(25, 23, '@', colors::WHITE);
-    let npc = Object::new(player.x - 1, player.y -3, '@', colors::YELLOW);
+    let (map, (player_x, player_y)) = make_map();
 
-    let mut objects = [player, npc];
-    let map = make_map();
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
+    // let npc = Object::new(player.x - 1, player.y -3, '@', colors::YELLOW);
+    let mut objects = [player];
+
 
     while !root.window_closed() {
         con.set_default_foreground(colors::WHITE);
@@ -137,20 +152,59 @@ fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Ma
     blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
 
-fn make_map() -> Map {
+fn make_map() -> (Map, (i32, i32)) {
     // Fill map with untraversable tiles
     // vec![ITEM;NUM] is a macro to create a Vec of size NUM filled with ITEM (where ITEM is
     // evaluated at each iteration).
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
+    let mut starting_position = (0, 0);
+    let mut rooms: Vec<Rect> = Vec::new();
 
-    create_x_tunnel(25, 55, 23, &mut map);
+    for _ in 0..MAX_ROOMS {
+        // Random width / height
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
 
-    map
+        // random position without going out of the boundaries of the map.
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        // If this room intersects with another one, it is considered invalid.
+        let invalid = rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+        if invalid {
+            continue;
+        }
+
+        create_room(new_room, &mut map);
+        let (new_x, new_y) = new_room.center();
+
+        if rooms.is_empty() {
+            // Player starts in first room, no tunnel needed.
+            starting_position = (new_x, new_y);
+        } else {
+            // All other rooms should be connected with the previous one.
+            let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+            // We randomly use tunnel_x or tunnel_y first.
+            if rand::random() {
+                create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                create_v_tunnel(prev_y, new_y, new_x, &mut map);
+            } else {
+                // first move vertically, then horizontally
+                create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                create_h_tunnel(prev_x, new_x, new_y, &mut map);
+            }
+        }
+
+        rooms.push(new_room);
+
+    }
+
+    (map, starting_position)
+
 }
 
 fn create_room(rect: Rect, map: &mut Map) {
@@ -161,13 +215,13 @@ fn create_room(rect: Rect, map: &mut Map) {
     }
 }
 
-fn create_x_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
     for x in min(x1, x2)..(max(x1, x2) + 1) {
         map[x as usize][y as usize] = Tile::empty();
     }
 }
 
-fn create_y_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
     for y in min(y1, y2)..(max(y1, y2) + 1) {
         map[x as usize][y as usize] = Tile::empty();
     }
