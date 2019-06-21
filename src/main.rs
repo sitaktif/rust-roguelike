62105b70e62019fff5e198fc98b62dffcd6f9c15
Romain Chossart <romainchossart@gmail.com>
2019-06-21 15:46:05 +0100

    Add inventory

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ const PANEL_Y: i32 = SCREEN_HEIGHT - PANEL_HEIGHT;
 const MSG_X: i32 = BAR_WIDTH + 2;
 const MSG_WIDTH: i32 = SCREEN_WIDTH - MSG_X;
 const MSG_HEIGHT: usize = PANEL_HEIGHT as usize - 1;
+const INVENTORY_WIDTH: i32 = 50;
 
 const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 6;
@@ -522,6 +523,71 @@ fn render_all(root: &mut Root,
     }
 }
 
+fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32, root: &mut Root) -> Option<usize> {
+    assert!(options.len() <= 26, "Cannot have a menu with more than 26 options.");
+
+    // Calculate the total height for the header (after auto-wrap) and one line per option.
+    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let height = options.len() as i32 + header_height;
+
+    // Create an off-screen console that represents the menu's window
+    let mut window = Offscreen::new(width, height);
+
+    // Print the header, with auto-wrap, and all the options, without wrap.
+    window.set_default_foreground(colors::WHITE);
+    window.print_rect_ex(0, 0, width, height, BackgroundFlag::None, TextAlignment::Left, header);
+
+    for (index, option_text) in options.iter().enumerate() {
+        let menu_letter = (b'a' + index as u8) as char;
+        let text = format!("{} - {}", menu_letter, option_text.as_ref());
+        window.print_ex(
+            0,
+            header_height + index as i32,
+            BackgroundFlag::None,
+            TextAlignment::Left,
+            text
+        );
+    }
+    // Blit the contents of "window" to the center of the root console.
+    // The last two params of blit are FG and BG transparency.
+    let x = SCREEN_WIDTH / 2 - width / 2;
+    let y = SCREEN_HEIGHT / 2 - height / 2;
+    tcod::console::blit(&mut window, (0, 0), (width, height), root, (x, y), 1.0, 0.7);
+
+    // Present the contents and wait for keypress (so that it doesn't disappear immediately).
+    root.flush();
+    let key = root.wait_for_keypress(true);
+
+    if key.printable.is_alphabetic() {
+        let index = key.printable.to_ascii_lowercase() as usize - 'a' as usize;
+        if index < options.len() {
+            Some(index)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn inventory_menu(inventory: &[Object], header: &str, root: &mut Root) -> Option<usize> {
+    // how a menu with each item of the inventory as an option
+    let options = if inventory.len() == 0 {
+        vec!["Inventory is empty.".into()]
+    } else {
+        inventory.iter().map(|item| { item.name.clone() }).collect()
+    };
+
+    let inventory_index = menu(header, &options, INVENTORY_WIDTH, root);
+
+    // if an item was chosen, return it
+    if inventory.len() > 0 {
+        inventory_index
+    } else {
+        None
+    }
+}
+
 fn render_messages(messages: &Messages, panel: &mut Offscreen) {
     let mut y = MSG_HEIGHT as i32;
     for &(ref msg, color) in messages.iter().rev() {
@@ -759,6 +825,15 @@ fn handle_keys(key: Key, root: &mut Root, map: &Map,
                 pick_item_up(item_id, objects, inventory, messages);
             }
             DidntTakeTurn
+        }
+
+        (Key { printable: 'i', .. }, true) => {
+            // show the inventory
+            inventory_menu(
+                inventory,
+                "Press the key next to an item to use it, or any other to cancel.\n",
+                root);
+            TookTurn
         }
 
         // Alt-enter: toggle fullscreen
